912143dd5524a05849389506268db51f9c3535f0
Sadeed Ahmad <sadeedahmadch@gmail.com>
2020-08-12 22:41:49 +1000

    first commit

---
+++ Sense Insight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sense Insight
+//
+//  Created by Sadeed Ahmed  on 12/8/20.
+//  Copyright © 2020 Sadeed Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
