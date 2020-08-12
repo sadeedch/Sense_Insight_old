c5b4f0227c4983fad0de6e09e7c35e40dcb3ada9
Sadeed Ahmad <sadeedahmadch@gmail.com>
2020-08-12 23:56:10 +1000

    gyroscope data added

--- Sense Insight/ViewController.swift
+++ Sense Insight/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var xGyro:UITextField!
+    @IBOutlet weak var yGyro:UITextField!
+    @IBOutlet weak var zGyro:UITextField!
+    
+    var motion = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myGyroscope()
     }
 
 
+    func myGyroscope() {
+        motion.gyroUpdateInterval = 0.5
+        motion.startGyroUpdates(to: OperationQueue.current!) { (data, error) in
+            print(data)
+            
+            if let trueData = data {
+                self.view.reloadInputViews()
+                let x = trueData.rotationRate.x
+                let y = trueData.rotationRate.y
+                let z = trueData.rotationRate.z
+                
+//                self.xGyro.text = "x: \(Double(x).rounded(toPlaces: 3))"
+//                self.yGyro.text = "y: \(Double(y).rounded(toPlaces: 3))"
+//                self.zGyro.text = "z: \(Double(z).rounded(toPlaces: 3))"
+                
+                self.xGyro.text = "x: \(Double(x).rounded(toPlaces: 3))"
+                self.yGyro.text = "y: \(y)"
+                self.zGyro.text = "z: \(y)"
+            }
+        }
+    }
+    
+}
+
+
+extension Double {
+    
+    func rounded(toPlaces places:Int) -> Double{
+        
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded()
+        
+    }
 }
 
